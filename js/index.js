c93de8dabd77663a243bd3ddc0bb0787ea4299a6
Layna Oyervides <laynaoyervides@gmail.com>
2021-09-30 17:43:48 -0600

    commit tree 0ca95299ee5160e6c49cb0759ff30573fac9d2de

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
+//main event
 document.addEventListener("DOMContentLoaded", function() {});
+//global variables 
+const allBooks = 'https://localhost:3000/books'; 
+const bookList = document.querySelector("#list");
+const showPanel = document.querySelector('#show-panel');
+
+//fetch request function
+//pillar 1
+function getBooks (){
+    fetch(allBooks){}
+    .then (resp => resp.json())
+    .then ((response) => {
+
+    })
+}
+//pillar 2 dom
+function turnBooksIntoHTML (response)
+let outerElement = document.createElement('li');
+outerElement.className = "book title";
+
+outerElement.innerText = ``
+
+//append
+
+bookList.append(outerElement)
